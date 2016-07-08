5826343806bc7ccc3616cc4c1cfe67b3303f4afd
j <jbaker@ffcsd.org>
2016-07-08 13:51:38 +0000

    updated session

--- signage/session.sh
+++ signage/session.sh
@@ -1,1 +1,8 @@
-test session
+#!/bin/bash
+
+x11vnc -rfbauth ~/schoolkit/signage/passwd -forever &
+
+while true; do
+     chromium "https://docs.google.com/a/ffcsd.org/document/d/1x7zY9sIPYGpIM9WIFsQyybs4_xQ2tpQphzYrYI_RlJ4/edit" --kiosk
+done
+
